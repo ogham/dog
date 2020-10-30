e0389e2968812085d253357bb185bc74c1cfc173
Benjamin Sago <ogham@bsago.me>
2020-10-30 21:11:49 +0000

    Add some shell completions

---
+++ completions/dog.bash
@@ -0,0 +1,48 @@
+_dog()
+{
+    cur=${COMP_WORDS[COMP_CWORD]}
+    prev=${COMP_WORDS[COMP_CWORD-1]}
+
+    case "$prev" in
+        -'?'|--help|-v|--version)
+            return
+            ;;
+
+        -t|--type)
+            COMPREPLY=( $( compgen -W 'A AAAA CAA CNAME HINFO MX NS PTR SOA SRV TXT' -- "$cur" ) )
+            return
+            ;;
+
+        --edns)
+            COMPREPLY=( $( compgen -W 'disable hide show' -- "$cur" ) )
+            return
+            ;;
+
+        -Z)
+            COMPREPLY=( $( compgen -W 'authentic' -- "$cur" ) )
+            return
+            ;;
+
+        --class)
+            COMPREPLY=( $( compgen -W 'IN CH HS' -- "$cur" ) )
+            return
+            ;;
+
+        --color|--colour)
+            COMPREPLY=( $( compgen -W 'always automatic never' -- $cur ) )
+            return
+            ;;
+    esac
+
+    case "$cur" in
+        -*)
+            COMPREPLY=( $( compgen -W '$( _parse_help "$1" )' -- "$cur" ) )
+            return
+            ;;
+
+        *)
+            COMPREPLY=( $( compgen -W 'A AAAA CAA CNAME HINFO MX NS PTR SOA SRV TXT' -- "$cur" ) )
+            ;;
+    esac
+} &&
+complete -o bashdefault -F _dog dog
---
+++ completions/dog.zsh
@@ -0,0 +1,26 @@
+#compdef dog
+
+__dog() {
+    _arguments \
+        "(- 1 *)"{-v,--version}"[Show version of dog]" \
+        "(- 1 *)"{-\?,--help}"[Show list of command-line options]" \
+        {-q,--query}"[Host name or IP address to query]::_hosts" \
+        {-t,--type}"[Type of the DNS record being queried]:(record type):(A AAAA CAA CNAME HINFO MX NS PTR SOA SRV TXT)" \
+        {-n,--nameserver}"[Address of the nameserver to send packets to]::_hosts;" \
+        --class"[Network class of the DNS record being queried]:(network class):(IN CH HS)" \
+        --edns"[Whether to OPT in to EDNS]:(edns setting):(disable hide show)" \
+        --txid"[Set the transaction ID to a specific value]" \
+        -Z"[Configure uncommon protocol tweaks]" \
+        {-U,--udp}"[Use the DNS protocol over UDP]" \
+        {-T,--tcp}"[Use the DNS protocol over TCP]" \
+        {-S,--tls}"[Use the DNS-over-TLS protocol]" \
+        {-H,--https}"[Use the DNS-over-HTTPS protocol]" \
+        {-1,--short}"[Display nothing but the finst result]" \
+        {-J,--json}"[Display the output as JSON]" \
+        {--color,--colour}"[When to use terminal colours]:(setting):(always automatic never)" \
+        --seconds"[Do not format durations, display them as seconds]" \
+        --time"[Print how long the response took to arrive"] \
+        '*:filename:_hosts'
+}
+
+__dog
